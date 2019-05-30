84bd94be86a869880a1ebd1a7e34a494906e7c7d
Manisha Patel <ma9shap.gmail.com>
2019-05-30 14:05:53 +0100

    step3

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
+
 class Battle < Sinatra::Application
+enable :sessions
 get '/' do
   'Testing infrastructure working!'
 end
@@ -9,8 +11,14 @@ get '/fill_form' do
 end
 
 post '/names' do
-  @names1 = params[:name1]
-  @names2 = params[:name2]
+  session[:names1] = params[:name1]
+  session[:names2] = params[:name2]
+  redirect to('/play')
+end
+
+get '/play' do
+  @names1 = session[:names1]
+  @names2 = session[:names2]
   erb(:play)
 end
 end
